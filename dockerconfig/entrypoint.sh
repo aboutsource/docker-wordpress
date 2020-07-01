be63b6b192ea5370beff05335ea0fc0049e2d248
Simon <simon.hoetten@aboutsource.net>
2020-07-01 16:57:15 +0200

    correct spelling error

--- dockerconfig/entrypoint.sh
+++ dockerconfig/entrypoint.sh
@@ -5,7 +5,7 @@ then
   sed ':b;/IfModule mpm_prefork_module/ba;n;bb;:a;s/\(MinSpareServers\).*/\1        '$APACHE_MPM_MINSPARESERVERS'/;n;/\/IfModule/b;ba' -i /etc/apache2/mods-available/mpm_prefork.conf
   sed ':b;/IfModule mpm_prefork_module/ba;n;bb;:a;s/\(MaxSpareServers\).*/\1        '$APACHE_MPM_MAXSPARESERVERS'/;n;/\/IfModule/b;ba' -i /etc/apache2/mods-available/mpm_prefork.conf
   sed ':b;/IfModule mpm_prefork_module/ba;n;bb;:a;s/\(MaxRequestWorkers\).*/\1      '$APACHE_MPM_MAXREQUESTWORKERS'/;n;/\/IfModule/b;ba' -i /etc/apache2/mods-available/mpm_prefork.conf
-  sed ':b;/IfModule mpm_prefork_module/ba;n;bb;:a;s/\(MaxConnectionsPerChild\).*/\1 '$APACHE_MPM_MAXCONNICTIONSPERCHILD'/;n;/\/IfModule/b;ba' -i /etc/apache2/mods-available/mpm_prefork.conf
+  sed ':b;/IfModule mpm_prefork_module/ba;n;bb;:a;s/\(MaxConnectionsPerChild\).*/\1 '$APACHE_MPM_MAXCONNECTIONSPERCHILD'/;n;/\/IfModule/b;ba' -i /etc/apache2/mods-available/mpm_prefork.conf
 fi
 
 exec $@
